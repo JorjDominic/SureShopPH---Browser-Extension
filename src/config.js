c7f8e24f441ee6fbf61b012aad6e40f32b71c235
JorjDominic <03jorgedmf12@gmail.com>
2026-05-23 07:01:04 +0800

    approval

--- src/config.js
+++ src/config.js
@@ -6,4 +6,4 @@
 //   - by popup.html as a <script> before popup.js
 //   - by all content scripts (listed first in manifest.json content_scripts)
 // -----------------------------------------------------------------------
-var SURESHOP_API_BASE = "http://localhost:8000";
+var SURESHOP_API_BASE = "https://sureshopph-backend-layer-production.up.railway.app";
